26c7fe8b795229eb532270eab88823f7acd13005
qwerty <jonathan.araya.m@gmail.com>
2024-12-23 03:16:33 -0300

    fix(layout): type

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,7 @@
 import Footer from "./Footer";
 import Header from "./Header";
 
-export default function Layout({ children }) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header />
